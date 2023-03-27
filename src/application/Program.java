05fd508c530f3c510dc540eedaf755abb7e224c8
Henrique Rafael Ardina <henrique.ardina@gmail.com>
2023-03-27 19:10:15 -0300

    First Class Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		
+		sc.close();
 	}
 
 }
