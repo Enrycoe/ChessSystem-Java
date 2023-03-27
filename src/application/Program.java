4253945d82fb5e2c00f949e6cf53ed81ed440148
Henrique Rafael Ardina <henrique.ardina@gmail.com>
2023-03-27 19:39:05 -0300

    Chess layer and printing the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,10 +2,15 @@ package application;
 
 import java.util.Scanner;
 
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 		
 		
 		sc.close();
