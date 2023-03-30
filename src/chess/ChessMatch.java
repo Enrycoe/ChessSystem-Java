418c833020736e5ac8e24b6102e7a1bdc3018c84
Henrique Rafael Ardina <henrique.ardina@gmail.com>
2023-03-29 22:13:25 -0300

    Possible Moves - Rook

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -28,6 +28,7 @@ public class ChessMatch {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPostion.toPosition();
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece)capturedPiece;
 	}
@@ -45,6 +46,12 @@ public class ChessMatch {
 		}
 		if(!board.piece(position).isThereAnyPossibleMoves()) {
 			throw new ChessException("There ir no possible moves for the chosen piece");
+		}
+	}
+	
+	private void validateTargetPosition(Position source, Position target) {
+		if(!board.piece(source).possibleMove(target)) {
+			throw new ChessException("The chosen piece can't move to target position");
 		}
 	}
 	
