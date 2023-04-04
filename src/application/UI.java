38d6fff02090f25572141b83c9c368972882e3cd
Henrique Rafael Ardina <henrique.ardina@gmail.com>
2023-04-03 21:12:56 -0300

    Switching player each turn

--- src/application/UI.java
+++ src/application/UI.java
@@ -3,6 +3,7 @@ package application;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -32,6 +33,14 @@ public class UI {
 		System.out.print("\033[H\033[2J");
 		System.out.flush();
 	}
+	
+	public static void printMatch(ChessMatch chessMatch) {
+		printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.println("Turn : " + chessMatch.getTurn());
+		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+	}
+	
 	
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
