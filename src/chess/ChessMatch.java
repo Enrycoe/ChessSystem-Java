c54765b96732b23666e1ec2a2e445a9f576f3d33
Henrique Rafael Ardina <henrique.ardina@gmail.com>
2023-04-04 21:05:23 -0300

    Bishop

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
+import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Pawn;
 import chess.pieces.Rook;
@@ -197,6 +198,7 @@ public class ChessMatch {
         placeNewPiece('d', 1, new Rook(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
         placeNewPiece('e', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
 
         placeNewPiece('b', 8, new Rook(board, Color.BLACK));
         placeNewPiece('d', 7, new Pawn(board, Color.BLACK));
